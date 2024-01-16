2577fae07ad95ddae7c9a239e04dac858036c91a
itgenie211 <150182623+itgenie211@users.noreply.github.com>
2024-01-16 17:57:18 -0500

    Add files via upload

---
+++ hair.js
@@ -0,0 +1,68 @@
+// Creating the simple data structures we'll work with.
+// The "hair" data will be simple information about
+// hair types.
+
+const hairstyles = [
+    {
+      hairStyle: "ponytail",
+      bestWith: "any",
+      cost: 0,
+      region: "Ancient Greece",
+      tools: ["brush" , "scrungie"]
+    },
+    {
+      hairStyle: "afro",
+      bestWith: "4a-d",
+      cost: "30/month",
+      region: "Tribal Africa",
+      tools: ["pick" , "leave-in conditioner"]
+    },
+    {
+        hairStyle: "braids",
+        bestWith: "any",
+        cost: "varies",
+        region: "Africa",
+        tools: ["gel" , "comb"]
+      },
+  ]
+  
+const hairColors = [
+     {
+       color: "blonde",
+       rarity: "2%",
+       shades: 50
+     },
+     {
+        color: "brown",
+        rarity: "11%",
+        shades: 26
+      },
+      {
+        color: "black",
+        rarity: "70%",
+        shades: 134
+      }
+  ] 
+
+const hairCuts = [
+    {
+        cut:"blunt bob",
+        longevity: "6 weeks",
+        symbolism: "feminism and individuality",
+        hairtype: "any"
+    },
+    {
+        hair:"pixie cut",
+        longevity: "5 weeks",
+        symbolism: "nonconformity and empowerment",
+        hairtype: "any"
+    },
+    {
+        hair:"finger waves",
+        longevity: "5 days",
+        symbolism: "Sophisticated",
+        hairtype: "any"
+    }
+]
+
+  module.exports = hair;
